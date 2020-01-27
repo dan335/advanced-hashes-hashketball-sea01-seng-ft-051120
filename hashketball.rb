5c7d323b613cacdffe347e031e0ef9e7a2f8151e
Daniel Phillips <danphi@gmail.com>
2020-01-27 05:22:16 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -154,7 +154,7 @@ end
 def team_colors(team_name)
   if (game_hash[:home][:team_name] == team_name)
     return game_hash[:home][:colors]
-  else if (game_hash[:away][:team_name] == team_name)
+  elsif (game_hash[:away][:team_name] == team_name)
     return game_hash[:away][:colors]
   end
 end
