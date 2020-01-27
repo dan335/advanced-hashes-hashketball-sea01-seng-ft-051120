724e191baaa71a60ce15159c0e5fe2ae4ae5029d
Daniel Phillips <danphi@gmail.com>
2020-01-27 05:06:24 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -8,13 +8,34 @@ def game_hash
       :players => [
         {
           :player_name => "Alan Anderson",
-          
+          :number => 0,
+          :shoe => 16,
+          :points => 22,
+          :rebounds => 12,
+          :assists => 12,
+          :steals => 3,
+          :blocks => 1,
+          :slam_dunks => 1
         }
         ]
     },
     :away => {
       :team_name => "Charlotte Hornets",
-      :colors => ["Turquoise", "Purple"]
+      :colors => ["Turquoise", "Purple"],
+      :players => [
+        {
+          :player_name => "Alan Anderson",
+          :number => 0,
+          :shoe => 16,
+          :points => 22,
+          :rebounds => 12,
+          :assists => 12,
+          :steals => 3,
+          :blocks => 1,
+          :slam_dunks => 1
+        }
+        ]
+    },
     }
   }
 end
